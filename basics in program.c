99aad431d4589d9f371c38293a3d59796f8ea00a
pkmr8242-gif <pkmr8242@gmail.com>
2026-05-11 13:58:49 +0530

    Add basic C program examples
    Add a C program that demonstrates counting down from 100, checking odd/even numbers, and printing odd numbers up to 10.

---
+++ basics in program.c
@@ -0,0 +1,33 @@
+// Online C compiler to run C program online
+/*#include <stdio.h>
+
+int main() {
+    
+    
+    for(int b=100;b>=1;b=b-1)
+    {
+       
+       printf("%d\n",b);
+    }
+    return 0;
+}
+
+#include <stdio.h>
+int main(){
+    int a;
+    scanf("%d",&a);
+    if(a%2 == 1){
+        printf("odd");
+    }
+    else{
+        printf("even");
+    }
+}
+
+#include <stdio.h>
+int main(){
+    for(int a=1;a<=10;a=a+2){
+        printf("%d\n",a);
+    }
+}
+*/
